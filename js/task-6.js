a49b79987392b79a1ba59ce39ce1264442294313
Vadim <pnevmat001@gmail.com>
2021-01-06 15:05:59 +0200

    fixes task 6 and task 7

--- js/task-6.js
+++ js/task-6.js
@@ -1,14 +1,16 @@
 const dataInputRef = document.querySelector('#validation-input');
-console.dir(dataInputRef);
+
 dataInputRef.addEventListener('input', () => {
-    if (dataInputRef.value.length < dataInputRef.dataset.length) {
-        dataInputRef.classList.add('invalid')
-        dataInputRef.classList.remove('valid')
-    } else if (dataInputRef.value.length > dataInputRef.dataset.length) {
-        dataInputRef.classList.add('invalid')
-        dataInputRef.classList.remove('valid')
-    } else {
-        dataInputRef.classList.add('valid')
-        dataInputRef.classList.remove('invalid')
+    if (dataInputRef.checked === false) {
+        if (dataInputRef.value.length < dataInputRef.dataset.length) {
+            dataInputRef.classList.add('invalid')
+            dataInputRef.classList.remove('valid')
+        } else if (dataInputRef.value.length > dataInputRef.dataset.length) {
+            dataInputRef.classList.add('invalid')
+            dataInputRef.classList.remove('valid')
+        } else {
+            dataInputRef.classList.add('valid')
+            dataInputRef.classList.remove('invalid')
+        }
     }
 });
