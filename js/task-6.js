06a10d1634769812c1d3a255aec184446a31feeb
Vadim <pnevmat001@gmail.com>
2021-01-06 14:46:53 +0200

    Swiches on task 7

--- js/task-6.js
+++ js/task-6.js
@@ -1,5 +1,14 @@
 const dataInputRef = document.querySelector('#validation-input');
-
+console.dir(dataInputRef);
 dataInputRef.addEventListener('input', () => {
-    dataInputRef.value.length < dataInputRef.dataset.length? dataInputRef.classList.add('invalid') : (dataInputRef.classList.add('valid'), dataInputRef.classList.remove('invalid'));
+    if (dataInputRef.value.length < dataInputRef.dataset.length) {
+        dataInputRef.classList.add('invalid')
+        dataInputRef.classList.remove('valid')
+    } else if (dataInputRef.value.length > dataInputRef.dataset.length) {
+        dataInputRef.classList.add('invalid')
+        dataInputRef.classList.remove('valid')
+    } else {
+        dataInputRef.classList.add('valid')
+        dataInputRef.classList.remove('invalid')
+    }
 });
